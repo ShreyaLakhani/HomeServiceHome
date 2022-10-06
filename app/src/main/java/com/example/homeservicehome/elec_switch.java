53d64e8deb690fc6b6535af1bd5808fcc7890ade
jeetmehta3012 <jeetmehta3012@gmail.com>
2022-10-06 13:08:52 +0530

    sub activity layouts created  :)

---
+++ app/src/main/java/com/example/homeservicehome/elec_switch.java
@@ -0,0 +1,14 @@
+package com.example.homeservicehome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class elec_switch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elec_switch);
+    }
+}

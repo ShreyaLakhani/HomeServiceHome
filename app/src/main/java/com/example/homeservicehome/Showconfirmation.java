75eba7c4f91c838a98c75e847116a24fb8b2bfc0
DhruviParekh <parekhdhruvi1012@gmail.com>
2022-11-07 23:22:24 +0530

    changes made to cleaning sub services -> provided headings to each activity!

--- app/src/main/java/com/example/homeservicehome/Showconfirmation.java
+++ app/src/main/java/com/example/homeservicehome/Showconfirmation.java
@@ -3,16 +3,12 @@ package com.example.homeservicehome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TimePicker;
 
-public class Finalcart extends AppCompatActivity {
-    TimePicker tp;
+public class Showconfirmation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_finalcart);
-        tp = findViewById(R.id.datePicker);
-        tp.setIs24HourView(true);
+        setContentView(R.layout.activity_showconfirmation);
     }
 }

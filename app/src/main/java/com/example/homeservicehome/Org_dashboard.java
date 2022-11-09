70fb69f9535588c03d1185a29508cd58e1551a42
DhruviParekh <parekhdhruvi1012@gmail.com>
2022-11-09 15:45:41 +0530

    changes made to all organization dashboard java file -> added list view id!

--- app/src/main/java/com/example/homeservicehome/Org_dashboard.java
+++ app/src/main/java/com/example/homeservicehome/Org_dashboard.java
@@ -3,12 +3,15 @@ package com.example.homeservicehome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class Org_dashboard extends AppCompatActivity {
+    ListView reqlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_org_dashboard);
+        reqlist = findViewById(R.id.requestlist);
     }
 }
